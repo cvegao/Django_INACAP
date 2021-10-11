efc4a9c1360aed113d05885b08ed3fe88ae750be
cvega <AvItala980>
2021-10-10 21:47:56 -0300

    sports orm 2 listo

--- sports_orm/leagues/views.py
+++ sports_orm/leagues/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
 
@@ -25,8 +25,27 @@ def index(request):
 		"jugadoresJoshua": Player.objects.filter(first_name="Joshua"),
 		"jugadoresCooperNoJoshua": Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua"),
 		"jugadoresAlexanderWyatt": Player.objects.filter(Q(first_name="Alexander") | Q(first_name="Wyatt")),
+		# PARTE 2
+		"equiposASC": Team.objects.filter(league__name="Atlantic Soccer Conference"),
+		"jugadoresBP": Team.objects.get(team_name="Penguins", location="Boston").curr_players.all(),
+		"jugadoresICBC": Player.objects.filter(curr_team__league__name="International Collegiate Baseball Conference"),
+		"jugadoresACAF": Player.objects.filter(last_name="Lopez").filter(
+			curr_team__league__name="American Conference of Amateur Football"),
+		"jugadoresFutbol": Player.objects.filter(curr_team__league__sport="Football"),
+		"equiposSophia": Team.objects.filter(curr_players__first_name="Sophia"),
+		"ligasSophia": League.objects.filter(teams__curr_players__first_name="Sophia"),
+		"jugadoresFloresNoWR": Player.objects.filter(last_name="Flores").exclude(curr_team__team_name="Roughriders", curr_team__location="Washington"),
+		"equiposSamuelE": Player.objects.get(first_name="Samuel", last_name="Evans").all_teams.all(),
+		"jugadoresMTC": Team.objects.get(team_name="Tiger-Cats", location="Manitoba").all_players.all(),
+		"jugadoresWV": Team.objects.get(team_name="Vikings", location="Wichita").all_players.all().exclude(curr_team__team_name="Vikings"),
+		"equiposJacobGray": Player.objects.get(first_name="Jacob", last_name="Gray").all_teams.all().exclude(curr_players__first_name="Jacob", curr_players__last_name="Gray"),
+		"joshuaAFABP": Player.objects.filter(first_name="Joshua", all_teams__league__name="Atlantic Federation of Amateur Baseball Players"),
+		"equiposSobre12": Team.objects.filter(all_players__gte=12),
+		"jugadoresEquipos": Player.objects.annotate(count=Count('all_teams')).order_by('-count'),
 	}
 	return render(request, "leagues/index.html", context)
+
+
 
 def make_data(request):
 	team_maker.gen_leagues(10)
